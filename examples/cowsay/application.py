36ad3e805db60a127bdd5f6d9bfa65e3ea1de4ca
Stefan Krawczyk <stefan@dagworks.io>
2024-02-13 16:32:22 -0800

    Adds image telephone example
    This uses the dataflows from the hub and then shows two ways
    to integrate Hamilton. One via the Hamilton integration, the other
    via the function API.
    
    Also adds tracking so that we can get some nice logs of what's going on!

--- examples/cowsay/application.py
+++ examples/cowsay/application.py
@@ -73,4 +73,4 @@ if __name__ == "__main__":
     app = application(in_terminal=True)
     app.visualize(output_file_path="digraph", include_conditions=True, view=True, format="png")
     while True:
-        action, result, state  = app.step()
+        action, result, state = app.step()
