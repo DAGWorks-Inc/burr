4455cf2d3b0d6a8b9bc09fc41eb94525ba36b3a1
elijahbenizzy <elijah_ben_izzy@alumni.brown.edu>
2024-03-05 18:59:50 -0800

    Changes from PR

--- telemetry/ui/src/components/common/href.tsx
+++ telemetry/ui/src/components/common/href.tsx
@@ -4,7 +4,14 @@
  */
 export const LinkText = (props: { href: string; text: string }) => {
   return (
-    <a href={props.href} className="text-dwlightblue hover:underline">
+    <a
+      href={props.href}
+      className="text-dwlightblue hover:underline"
+      onClick={(e) => {
+        // Quick trick to ensure that this takes priority and if this has a parent href, it doesn't trigger
+        e.stopPropagation();
+      }}
+    >
       {props.text}
     </a>
   );
