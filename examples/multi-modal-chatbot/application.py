1155c44540f1892d52cd9933b7d70d2cc2ee0ea4
elijahbenizzy <elijah_ben_izzy@alumni.brown.edu>
2024-03-30 11:04:18 -0700

    Makes multi-modal-chatbot compliant

--- examples/multi-modal-chatbot/application.py
+++ examples/multi-modal-chatbot/application.py
@@ -3,11 +3,9 @@ import os
 from typing import List, Optional, Tuple
 
 import openai
-from hamilton import driver
 
 from burr.core import Application, ApplicationBuilder, State, default, when
 from burr.core.action import action
-from burr.integrations.hamilton import Hamilton, append_state, from_state, update_state
 from burr.lifecycle import LifecycleAdapter
 from burr.tracking import LocalTrackingClient
 
@@ -200,90 +198,15 @@ def base_application(
     )
 
 
-def hamilton_application(
-    hooks: List[LifecycleAdapter], app_id: str, storage_dir: str, project_id: str
-):
-    import dag
-
-    dr = driver.Driver({"provider": "openai"}, dag)  # TODO -- add modules
-    Hamilton.set_driver(dr)
-    application = (
-        ApplicationBuilder()
-        .with_state(chat_history=[], prompt="Draw an image of a turtle saying 'hello, world'")
-        .with_entrypoint("prompt")
-        .with_state(chat_history=[])
-        .with_actions(
-            prompt=Hamilton(
-                inputs={"prompt": from_state("prompt")},
-                outputs={"processed_prompt": append_state("chat_history")},
-            ),
-            check_safety=Hamilton(
-                inputs={"prompt": from_state("prompt")},
-                outputs={"safe": update_state("safe")},
-            ),
-            decide_mode=Hamilton(
-                inputs={"prompt": from_state("prompt")},
-                outputs={"mode": update_state("mode")},
-            ),
-            generate_image=Hamilton(
-                inputs={"prompt": from_state("prompt")},
-                outputs={"generated_image": update_state("response")},
-            ),
-            generate_code=Hamilton(  # TODO -- implement
-                inputs={"chat_history": from_state("chat_history")},
-                outputs={"generated_code": update_state("response")},
-            ),
-            answer_question=Hamilton(  # TODO -- implement
-                inputs={"chat_history": from_state("chat_history")},
-                outputs={"answered_question": update_state("response")},
-            ),
-            prompt_for_more=Hamilton(
-                inputs={},
-                outputs={"prompt_for_more": update_state("response")},
-            ),
-            response=Hamilton(
-                inputs={
-                    "response": from_state("response"),
-                    "safe": from_state("safe"),
-                    "mode": from_state("mode"),
-                },
-                outputs={"processed_response": append_state("chat_history")},
-            ),
-        )
-        .with_transitions(
-            ("prompt", "check_safety", default),
-            ("check_safety", "decide_mode", when(safe=True)),
-            ("check_safety", "response", default),
-            ("decide_mode", "generate_image", when(mode="generate_image")),
-            ("decide_mode", "generate_code", when(mode="generate_code")),
-            ("decide_mode", "answer_question", when(mode="answer_question")),
-            ("decide_mode", "prompt_for_more", default),
-            (
-                ["generate_image", "answer_question", "generate_code", "prompt_for_more"],
-                "response",
-            ),
-            ("response", "prompt", default),
-        )
-        .with_hooks(*hooks)
-        .with_identifiers(app_id=app_id)
-        .with_tracker("local", project=project_id, params={"storage_dir": storage_dir})
-        .build()
-    )
-    return application
-
-
 def application(
-    use_hamilton: bool,
     app_id: Optional[str] = None,
     project_id: str = "demo:chatbot",
     storage_dir: Optional[str] = "~/.burr",
     hooks: Optional[List[LifecycleAdapter]] = None,
 ) -> Application:
-    if use_hamilton:
-        return hamilton_application(hooks, app_id, storage_dir, project_id=project_id)
     return base_application(hooks, app_id, storage_dir, project_id=project_id)
 
 
 if __name__ == "__main__":
-    app = application(use_hamilton=False)
+    app = application()
     app.visualize(output_file_path="digraph", include_conditions=False, view=True, format="png")
