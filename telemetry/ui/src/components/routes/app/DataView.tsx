28607c4447beb5594f4052d87861a24c4969bdd4
Stefan Krawczyk <stefan@dagworks.io>
2024-03-31 16:04:43 -0700

    Adds MVP form rendering
    This is pretty basic -- but it seems to work.
    
    This handles:
     - null values
     - arrays
     - dicts/objects
    
    Tries to intelligently provide indexing (but
    for objects it could be better). Ideally we'd
    display the JSON path or something...

--- telemetry/ui/src/components/routes/app/DataView.tsx
+++ telemetry/ui/src/components/routes/app/DataView.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Step } from '../../../api';
 import JsonView from '@uiw/react-json-view';
 import { Button } from '../../common/button';
-import { useState } from 'react';
 
 const StateButton = (props: { label: string; selected: boolean; setSelected: () => void }) => {
   const color = props.selected ? 'zinc' : 'light';
@@ -81,7 +80,13 @@ export const DataView = (props: { currentStep: Step | undefined; priorStep: Step
           <ErrorView error={error} />
         </>
       )}
-      {resultData && (
+      {resultData && viewRawData === 'render' && (
+        <>
+          <h1 className="text-2xl text-gray-600 font-semibold">Result</h1>
+          <FormRenderer data={resultData} />
+        </>
+      )}
+      {resultData && viewRawData === 'raw' && (
         <>
           <h1 className="text-2xl text-gray-600 font-semibold">Result</h1>
           <JsonView value={resultData} collapsed={2} enableClipboard={false} />
@@ -97,28 +102,86 @@ export const DataView = (props: { currentStep: Step | undefined; priorStep: Step
   );
 };
 
+interface StringComponentProps {
+  name: string;
+  value: string;
+  level: number;
+}
+// String component is used to render string data
+const StringComponent: React.FC<StringComponentProps> = ({ name, value, level }) => {
+  const renderField = (name: string, value: string, level: number) => {
+    return (
+      <div key={name + '-' + String(level)} className="border">
+        <label className="border text-xl font-semibold">{name}</label>
+        <br />
+        <pre
+          className=""
+          style={{ whiteSpace: 'pre-wrap', wordWrap: 'break-word', maxWidth: '1000px' }}
+        >
+          {value}
+        </pre>
+      </div>
+    );
+  };
+  return <div>{renderField(name, value, level)}</div>;
+};
+
 interface FormRendererProps {
   data: Record<string, string | number | boolean | object>;
 }
+
+// This component is used to render the form data in a structured way
 const FormRenderer: React.FC<FormRendererProps> = ({ data }) => {
-  const renderField = (value: string | number | boolean | object, key: string) => {
+  const renderField = (value: string | number | boolean | object, key: string, level: number) => {
+    // TODO: have max level depth.
     if (typeof value === 'string') {
+      return <StringComponent name={key} value={value} level={level} />;
+    } else if (Array.isArray(value)) {
       return (
-        <div key={key} className="border">
-          <label className="border">{key}</label>
+        <div key={key + '-' + String(level)} className="border">
+          <label className="border text-xl font-semibold">{key}</label>
           <br />
-          <pre
-            className=""
-            style={{ whiteSpace: 'pre-wrap', wordWrap: 'break-word', maxWidth: '1000px' }}
-          >
-            {value}
-          </pre>
+          <div>
+            {value.map((v, i) => {
+              return (
+                <div key={key + '-' + i.toString()} className="border-2">
+                  {renderField(v, key + '-' + i.toString(), level + 1)}
+                </div>
+              );
+            })}
+          </div>
         </div>
       );
-    } else {
+    } else if (typeof value === 'object') {
       return (
         <div key={key} className="border">
-          <label className="border">{key}</label>
+          <label className="border text-xl font-semibold">{key}</label>
+          <br />
+          <div>
+            {value === null ? (
+              <span>NULL</span>
+            ) : (
+              Object.entries(value).map(([k, v]) => {
+                return (
+                  <div key={key + '-' + k} className="border-2">
+                    {renderField(v, k, level + 1)}
+                  </div>
+                );
+              })
+            )}
+          </div>
+        </div>
+      );
+    } else if (value === null) {
+      <div key={key + '-' + String(level)} className="border">
+        <label className="border text-xl font-semibold">{key}</label>
+        <br />
+        <span>NULL</span>
+      </div>;
+    } else {
+      return (
+        <div key={key + '-' + String(level)} className="border">
+          <label className="border text-xl font-semibold">{key}</label>
           <br />
           <span>{value.toString()}</span>
         </div>
@@ -127,7 +190,10 @@ const FormRenderer: React.FC<FormRendererProps> = ({ data }) => {
   };
 
   const renderFields = () => {
-    return Object.entries(data).map(([key, value]) => renderField(value, key));
+    if (data !== null) {
+      return Object.entries(data).map(([key, value]) => renderField(value, key, 0));
+    }
+    return null;
   };
 
   return <div>{renderFields()}</div>;
