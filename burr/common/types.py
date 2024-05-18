d11f35568ecfe70e2b046f5743b9f32c8f1291ef
elijahbenizzy <elijah_ben_izzy@alumni.brown.edu>
2024-05-18 13:24:58 -0700

    Adds note on types.py
    This is a bit ugly, but its a placeholder for a much cleaner future
    refactor. I stand by haviung types.py here so we can centralize future
    ones, even though only ParentPointer is there now.

--- burr/common/types.py
+++ burr/common/types.py
@@ -2,6 +2,10 @@ import dataclasses
 from typing import Optional
 
 
+# This contains commmon types
+# Currently the types are a little closer to the logic than we'd like
+# We'll want to break them out into interfaces and put more here eventually
+# This will help avoid the ugly if TYPE_CHECKING imports;
 @dataclasses.dataclass
 class ParentPointer:
     app_id: str
