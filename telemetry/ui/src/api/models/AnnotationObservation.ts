b657f87106d4722be856ccb1db9cb2c7ad090a11
elijahbenizzy <elijah_ben_izzy@alumni.brown.edu>
2024-10-08 14:33:07 -0700

    Adds data models + BE integrations for annotations
    1. Exposes as mixin in the BE
    2. Adds GET/POST/PUT endpoints
    3. Creates data models
    
    Does not work with s3 yet.

---
+++ telemetry/ui/src/api/models/AnnotationObservation.ts
@@ -0,0 +1,10 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { AnnotationDataPointer } from './AnnotationDataPointer';
+export type AnnotationObservation = {
+  data_fields: Record<string, any>;
+  thumbs_up_thumbs_down: boolean | null;
+  data_pointers: Array<AnnotationDataPointer>;
+};
